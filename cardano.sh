5c42205124e1ecb99e5306adea8665299129df34
damianruda <40346606+damianruda@users.noreply.github.com>
2021-06-25 15:22:41 -0300

    Update and rename cardano.sh to veruscoin.sh

--- cardano.sh
+++
@@ -1,5 +0,0 @@
-# cardano
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
-tar -xf xmrig-6.12.1-linux-x64.tar.gz
-./xmrig -o rx.unmineable.com:3333 -a rx/o -k -u ADA:addr1q9mujnnz5ks4m32lvqwzjgvtxr7ghaf5zymx53euczzreznhe98x9fdpthz47cqu9ysckv8u306ngyfkdfrnesyy8j9qhw2ph9.slash
